2e313439f0d8545168c85fa369831d4d7bfb7c58
Rohan <139545262+ForgottenRohan@users.noreply.github.com>
2025-03-01 19:37:18 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "7771154906:AAGOZnoze9DPFS8ddGQ71r0dAkuBqqQzqdI"  # токен ботика
+CHANNEL_URL = "https://t.me/millonerrrrrrr"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002072322896"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002435215340"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/supporw1n"
+ADMIN_ID = 1400424377 # Ваш айди без скобочек
+PROMO = "MACKVIN" # промокод
